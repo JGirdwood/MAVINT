a7dc54f214d3749c209187a90011920013646074
JGirdwood <42206925+JGirdwood@users.noreply.github.com>
2018-12-06 22:35:54 +0000

    Added example
    NB: not for use with arduino uno since it does not have enough memory.

---
+++ examples/LOGGER_JG_181126/LOGGER_JG_181126.ino
@@ -0,0 +1,177 @@
+/*******************************************************************************************
+This program is intended for use with an Arduino UNO with an SD card shield to log UCASS and
+MET data. The ICSP pins are used for communication with both the UCASS and the SD card. The 
+chip select pin for the UCASS must be opperated at 3.3v so a resistor divider must be utilised.
+
+V3 includes options for interfacing with pixhawk using the 'mavlink' library designed by Lorenz
+Meier, here it is used for position and pressure information.
+
+Author: Joe Girdwood
+Email: j.girdwood@herts.ac.uk
+********************************************************************************************/
+
+//Libraries
+#include <MET.h>
+#include <UCASS.h>
+#include <MAVINT.h>
+#include <SoftwareSerial.h>
+
+//Variables
+const bool useUCASS = true;               //Using UCASS and pixhawk?
+const bool echotoserial = true;           //Set to true if a serial output is desired
+const int res = 500;                      //Resolution of measurements
+#define mon Serial                        //Serial data stream
+const int baud = 57600;                   //Serial baud rate
+
+//Pins
+const char sdLEDpin = 2;                  //LED used for errors
+const char HIH4000pin = A3;               //Input from HIH4000
+const char DCRESpin = A2;                 //DC restoration pin
+const char bridgeHIpin = A1;              //thermistor bridge pin
+const char bridgeLOpin = A0;              //ditto
+const char SDcs = 10;                     //SD card chip select
+const char UCASScs = 9;                   //UCASS chip select
+
+//Instantiators 
+MET met;                                  //Instantiator for MET class
+UCASS ucass;                              //Instantiator for UCASS class
+MAVINT mavint;                            //Instantiator for MAVINT class
+
+void setup() {
+  
+  //Setting up Pins
+  pinMode(sdLEDpin,OUTPUT);
+  pinMode(DCRESpin,INPUT);
+  pinMode(bridgeHIpin,INPUT);
+  pinMode(bridgeLOpin,INPUT);
+  pinMode(HIH4000pin,INPUT);
+  pinMode(SDcs,OUTPUT);
+  if (useUCASS == true){
+    pinMode(UCASScs,OUTPUT);
+    digitalWrite(UCASScs,HIGH);
+  }else{
+    pinMode(UCASScs,INPUT);
+    digitalWrite(UCASScs,LOW);
+  }
+
+  mon.begin(baud);
+  while(!mon){}
+  if (useUCASS == true){
+    Serial1.begin(baud);
+    while(!Serial1){}
+  }
+  
+  bool SDOK = met.startSD(SDcs, sdLEDpin, mon);     //Setting up SD card
+  bool logOK = met.createlog(mon);                  //Creating Log File
+  bool RTCOK = met.startRTC(mon);                   //Setting up RTC
+  bool SPIOK = true;
+  if (useUCASS == true){
+    SPIOK = ucass.startSPI(mon);
+  }
+  bool err = SDOK*logOK*RTCOK*SPIOK;                //Determining error
+  met.error(err, mon, sdLEDpin);                    //Acting on error
+  met.setupMonitor(mon);                            //Setting up files
+  
+  if (useUCASS == true){
+    ucass.readInfoString(UCASScs,mon);
+    ucass.printINFOstr(mon);
+    met.openLog(mon);
+    ucass.printINFOstr(met.Log);
+    met.closeLog(SDcs);
+
+    ucass.readConfigVars(UCASScs,mon);
+    mon.println();
+    mon.println(F("bb0\tbb1\tbb2\tbb3\tbb4\tbb5\tbb6\tbb7\tbb8\tbb9\tbb10\tbb11\tbb12\tbb13\tbb14\tbb15\tGSC\tID"));
+    ucass.printConfig(mon, '\t');
+    met.openLog(mon);
+    met.Log.println(F("bb0,bb1,bb2,bb3,bb4,bb5,bb6,bb7,bb8,bb9,bb10,bb11,bb12,bb13,bb14,bb15,GSC,ID"));
+    ucass.printConfig(met.Log, ',');
+    met.closeLog(SDcs);
+  
+    mon.println();
+    mon.println(F("time\ttemp\thum\tb0\tb1\tb2\tb3\tb4\tb5\tb6\tb7\tb8\tb9\tb10\tb11\tb12\tb13\tb14\tb15\tb1tof\tb3tof\tb5tof\tb7tof\tperiod\tcsum\tglitch\tlongtof\trejrat\tpres\tlat\tlon\tgpsalt\tgpsvz"));
+    met.openLog(mon);
+    met.Log.println(F("time,temp,hum,b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b1tof,b3tof,b5tof,b7tof,period,csum,glitch,longtof,rejrat,pres,lat,lon,gpsalt,gpsvz"));
+    met.closeLog(SDcs);
+  }else{
+    met.openLog(mon);
+    mon.println(F("time\ttemp\thum"));
+    met.Log.println(F("time,temp,hum"));
+    met.closeLog(SDcs);
+  }
+}
+
+void loop() {
+  
+  //Defining Temporal Resolution
+  delay(res);
+  unsigned long t = millis();
+
+  //Requesting MAVlink data
+  if (useUCASS == true){
+    mavint.MAV_sendHB();
+    Serial1.write(mavint.buf,mavint.len);
+    mavint.reqStream();
+    Serial1.write(mavint.buf,mavint.len);
+  }
+
+  //Reading
+  if (useUCASS == true){ 
+    ucass.readHistData(UCASScs, mon);
+  }
+  met.METread(HIH4000pin, bridgeHIpin, bridgeLOpin);
+  
+  //Printing
+  if (echotoserial == true){
+    mon.print(t);
+    mon.print('\t');
+    met.METprint(mon, '\t');
+    if (useUCASS == true){
+      mon.print('\t');
+      ucass.printHist(mon,'\t');
+    }
+    mon.println();
+  }
+  
+  met.openLog(mon);
+  met.Log.print(t);
+  met.Log.print(',');
+  met.METprint(met.Log, ',');
+  if (useUCASS == true){
+    met.Log.print(',');
+    ucass.printHist(met.Log, ',');
+  }
+
+  //Processing MAVlink
+  if (useUCASS == true){
+    while(Serial1.available()>0) {
+      uint8_t MAVin = Serial1.read();
+      mavint.getMAVpres(MAVin);
+      met.Log.print(mavint.MAVdat);
+      mavint.getMAVlat(MAVin);
+      met.Log.print(mavint.MAVdat);
+      mavint.getMAVlon(MAVin);
+      met.Log.print(mavint.MAVdat);
+      mavint.getMAVgpsalt(MAVin);
+      met.Log.print(mavint.MAVdat);
+      mavint.getMAVgpsvz(MAVin);
+      met.Log.print(mavint.MAVdat);
+    }
+  }
+  
+  met.closeLog(SDcs);
+  met.Log.println();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
